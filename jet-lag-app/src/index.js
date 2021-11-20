de6f1c88de7b2300a77402f38403bc6a01fea950
bwolf02 <bwolf2@uwo.ca>
2021-11-20 15:40:15 -0500

    Building express endpoints

--- jet-lag-app/src/index.js
+++ jet-lag-app/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('static'));
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.post('/getSchedule', (req, res) => {
+  req.body
+})
 
 
 ReactDOM.render(
